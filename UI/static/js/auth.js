85bbf9e3ff6ab3261d17937569e35310226369be
nduhiu17 <nduhiu254@gmail.com>
2019-03-04 10:52:02 +0300

    featute(sign up)A user should be able to sign up - create function that collects data from sign up form - create function that hits the api with registration data - create function that displays messages - link js with html - update css [Finishes #164331197]

--- UI/static/js/auth.js
+++ UI/static/js/auth.js
@@ -34,3 +34,56 @@ let login = () => {
             }
     }})
 }
+
+const registerApi = (data)=>{//function that hits api endpoint with the data collected
+    return fetch('https://politico-api-server.herokuapp.com/api/v2/auth/signup', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+
+        })
+        .then((res) =>{
+            return res.json()
+        })
+};
+
+
+
+let register = () => {//function that collects registration details
+    let firstname = document.getElementById('signup-fname').value;
+    let lastname = document.getElementById('signup-lname').value;
+    let othername = document.getElementById('signup-oname').value;
+    let email = document.getElementById('signup-email').value;
+    let phonenumber = document.getElementById('signup-phone').value;
+    let county = document.getElementById('signup-county').value;
+    let nationalid = document.getElementById('signup-nationalid').value;
+    let passporturl = document.getElementById('signup-passporturl').value;
+    let password = document.getElementById('signup-password').value;
+    let rpassword = document.getElementById('signup-rpassword').value;
+    let data = {//asigning variable to sign up details
+        email: email,
+        password: password,
+        firstname:firstname,
+        lastname:lastname,
+        othername:othername,
+        phonenumber:phonenumber,
+        county:county,
+        nationalid:nationalid,
+        passporturl:passporturl,
+        repeatpassword:rpassword
+    }
+
+    registerApi(data).then(res=> {
+        if(res.error){
+            window.alert(res.error)
+        }else {
+            console.log(res)
+            localStorage.setItem("token",res.token)
+            if (res.message === "User registered") {//checking a successful registration
+                window.location.replace("index.html");//redirecting to index page after a successful registration
+            }
+    }})
+}
